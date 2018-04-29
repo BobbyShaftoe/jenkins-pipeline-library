84a32d31c75ce1fc9f933cc7c45f2eb4949075c6
nicksinclair <tech@halcyonnetworks.com.au>
2018-04-30 02:36:59 +0800

    Rename files

---
+++ vars/setupCheck.groovy
@@ -0,0 +1,40 @@
+def call(body) {
+
+ def config = [: ]
+ body.resolveStrategy = Closure.DELEGATE_FIRST
+ body.delegate = config
+ body()
+
+ node('aws-node-00') {
+  // Clean workspace before doing anything
+  deleteDir()
+
+  try {
+   stage('Checkout main repository') {
+    git poll: false, url: 'https://github.com/BobbyShaftoe/pipeline-job-aa.git'
+   }
+
+   stage('List files in workspace') {
+    sh 'ls -larst .'
+    sh 'ls -la */*'
+   }
+
+   stage('Environment') {
+    sh 'scripts/env.sh'
+
+   }
+   stage('Read placeholder') {
+    readFile 'lib/placeholder.txt'
+   }
+
+   stage('Archive workspace') {
+     archiveArtifacts '**/*.*'
+   }
+
+  } catch (err) {
+   currentBuild.result = 'FAILED'
+   throw err
+  }
+
+ }
+}
--- vars/test.groovy
+++ vars/test.groovy
@@ -1,2 +1,5 @@
 
 // Placeholder
+withEnv(['JRE_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.161-0.b14.amzn2.x86_64/jre']) {
+    // some block
+}
