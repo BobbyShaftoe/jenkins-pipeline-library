0f4dda534080c90607e186873f02c5f905564970
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 17:48:48 +0800

    Use in stage

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,8 +1,25 @@
+def call(body) {
 
-def call(String version) {
-  infoBuildNumber = env.BUILD_NUMBER
-  infoJobName = env.JOB_NAME
-  infoSharedLib = 'jenkins-pipeline-library'
-  echo "$infoSharedLib"
+ def config = [: ]
+ body.resolveStrategy = Closure.DELEGATE_FIRST
+ body.delegate = config
+ body()
+
+  node('aws-node-00') {
+    try {
+
+        stage('Version Info') {
+          infoBuildNumber = env.BUILD_NUMBER
+          infoJobName = env.JOB_NAME
+          infoSharedLib = 'jenkins-pipeline-library'
+          echo "$infoSharedLib:$infoBuildNumber:$infoJobName"
+        }
+
+    } catch (err) {
+   currentBuild.result = 'FAILED'
+   throw err
+    }
+  }
 }
+
 
