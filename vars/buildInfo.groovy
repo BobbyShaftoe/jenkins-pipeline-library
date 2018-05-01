ee8ed6f439ed4e10e9f188b3836cbaecd1483ba1
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 17:59:12 +0800

    Revert to call method

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,25 +1,12 @@
-def call(body) {
 
- def config = [: ]
- body.resolveStrategy = Closure.DELEGATE_FIRST
- body.delegate = config
- body()
-
-  node('aws-node-00') {
-    try {
-
-        stage('Version Info') {
-          infoBuildNumber = env.BUILD_NUMBER
-          infoJobName = env.JOB_NAME
-          infoSharedLib = 'jenkins-pipeline-library'
-          echo "$infoSharedLib:$infoBuildNumber:$infoJobName"
-        }
-
-    } catch (err) {
-   currentBuild.result = 'FAILED'
-   throw err
-    }
-  }
+def buildinfo() {
+  infoBuildNumber = env.BUILD_NUMBER
+  infoJobName = env.JOB_NAME
+  echo "$infoBuildNumber $infoJobName"
 }
 
+def libinfo() {
+  infoSharedLib = 'jenkins-pipeline-library'
+  echo "$infoSharedLib"
+}
 
