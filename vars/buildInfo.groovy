0d826938a07b1c06f195a43ddcac126a5c0ee84e
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 19:07:27 +0800

    Remove sigil

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -3,7 +3,7 @@ def call(String stagename = '') {
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
   infoSharedLib = 'jenkins-pipeline-library'
-  env.SHARED_LIB = $infoSharedLib
+  env.SHARED_LIB = infoSharedLib
   echo "('$stagename') SharedLib: $infoSharedLib"
 }
 
