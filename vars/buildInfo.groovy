4b333882d0499d56fa8d1403c2306234c5d7f760
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 18:18:20 +0800

    Fix square bracket

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -3,6 +3,6 @@ def call(String stagename = '') {
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
   infoSharedLib = 'jenkins-pipeline-library'
-  echo ["$stagename] Using shared lib: $infoSharedLib"
+  echo "[$stagename] Using shared lib: $infoSharedLib"
 }
 
