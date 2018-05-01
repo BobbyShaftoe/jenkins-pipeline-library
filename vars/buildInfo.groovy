6c818bba4a9bb7a0ca2ff678ae87f23228139d8c
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 18:16:08 +0800

    Add stage name var

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,8 +1,8 @@
 
-def call(String version = '') {
+def call(String stagename = '') {
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
   infoSharedLib = 'jenkins-pipeline-library'
-  echo "  Using shared lib: $infoSharedLib"
+  echo ["$stagename] Using shared lib: $infoSharedLib"
 }
 
