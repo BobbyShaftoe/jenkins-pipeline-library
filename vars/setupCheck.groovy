0cc86e250de47bededae73e64a8a39e2ea7dfa5f
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 18:25:22 +0800

    Change message format

--- vars/setupCheck.groovy
+++ vars/setupCheck.groovy
@@ -11,7 +11,7 @@ def call(body) {
 
   try {
    stage('Checkout main repository') {
-    buildInfo('Run the env script')
+    buildInfo('Checkout main repository')
     git poll: false, url: 'https://github.com/BobbyShaftoe/pipeline-job-aa.git'
    }
 
