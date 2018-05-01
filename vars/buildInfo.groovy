c204c52cbccac28500123fbf875bc30c64107296
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 19:09:35 +0800

    Move shared lib var

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,8 +1,10 @@
 
+infoSharedLib = 'jenkins-pipeline-library'
+
+
 def call(String stagename = '') {
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
-  infoSharedLib = 'jenkins-pipeline-library'
   env.SHARED_LIB = infoSharedLib
   echo "('$stagename') SharedLib: $infoSharedLib"
 }
