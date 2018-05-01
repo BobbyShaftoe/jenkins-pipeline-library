9a9d47e2b7fc4ea78b2de019edfb6f4852505cfe
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-01 17:16:52 +0800

    Add build info lib

---
+++ vars/buildInfo.groovy
@@ -0,0 +1,11 @@
+def buildinfo() {
+  infoBuildNumber = env.BUILD_NUMBER
+  infoJobName = env.JOB_NAME
+  echo "$infoBuildNumber $infoJobName"
+}
+
+def libinfo() {
+  infoSharedLib = 'jenkins-pipeline-library'
+  echo "$infoSharedLib"
+}
+
