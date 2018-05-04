4813369a01f0fda3e29fe78b5eb48ae92d0fbacc
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-04 21:37:34 +0800

    Update setup check

--- vars/setupCheck.groovy
+++ vars/setupCheck.groovy
@@ -7,40 +7,33 @@ def call(body) {
 
   node('aws-node-00') {
     ansiColor('xterm') {
-      // Clean workspace before doing anything
-      //deleteDir()
 
       try {
-       stage('Checkout main repository') {
-        buildInfo('Checkout main repository')
-        //git poll: false, url: 'https://github.com/BobbyShaftoe/pipeline-job-aa.git'
-        sh 'echo "not checking out here"'
+
+        stage('List files in workspace') {
+          buildInfo('List files in workspace')
+          sh 'ls -larst .'
+          sh 'ls -la */*'
        }
 
-       stage('List files in workspace') {
-        buildInfo('List files in workspace')
-        sh 'ls -larst .'
-        sh 'ls -la */*'
-       }
+        stage('Environment') {
+          buildInfo('Environment')
+          sh 'scripts/env.sh'
 
-       stage('Environment') {
-        buildInfo('Environment')
-        sh 'scripts/env.sh'
+         }
+        stage('Read placeholder') {
+          buildInfo('Read placeholder')
+          readFile 'lib/placeholder.txt'
+         }
 
-       }
-       stage('Read placeholder') {
-        buildInfo('Read placeholder')
-        readFile 'lib/placeholder.txt'
-       }
-
-       stage('Archive workspace') {
-         buildInfo('Archive workspace')
-         archiveArtifacts '**/*.*'
-       }
+        stage('Archive workspace') {
+           buildInfo('Archive workspace')
+           archiveArtifacts '**/*.*'
+         }
 
       } catch (err) {
-       currentBuild.result = 'FAILED'
-       throw err
+          currentBuild.result = 'FAILED'
+          throw err
       }
     }
   }
