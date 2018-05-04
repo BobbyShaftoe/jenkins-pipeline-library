26a58ae69093c0c9303f5585de066b90dd00edba
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-04 20:38:26 +0800

    Remove argument

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,8 +1,9 @@
 
-infoSharedLib = 'jenkins-pipeline-library'
 
 
 def call(String stagename = '') {
+  infoSharedLib = 'jenkins-pipeline-library'
+
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
   env.SHARED_LIB = infoSharedLib
