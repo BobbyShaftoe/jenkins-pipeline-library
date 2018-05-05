1932acfb2f548e39a793370ca0557f1efd7ff907
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-05 17:43:03 +0800

    Message string var

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -5,7 +5,9 @@ def call(String messageString = '') {
 
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
+  stageName = env.STAGE_NAME
   env.SHARED_LIB = infoSharedLib
-  echo "[Pipeline] \u001B[31mStage('$env.STAGE_NAME')\u001B[0m \""$messageString"\" \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
+
+  echo "[Pipeline] \u001B[31mStage("$stageName")\u001B[0m \""$messageString"\" \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
 }
 
