6950215be7f8bd815aa1e2cb57eb3c5341f435bb
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-05 17:08:20 +0800

    Update build info method

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -6,6 +6,6 @@ def call(String stagename = '') {
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
   env.SHARED_LIB = infoSharedLib
-  echo "('$stagename') SharedLib: $infoSharedLib"
+  echo "[Pipeline] Stage ('$stagename') SharedLib: $infoSharedLib"
 }
 
--- vars/getAWSDetails.groovy
+++ vars/getAWSDetails.groovy
@@ -1,4 +1,5 @@
 def call() {
+  buildInfo('Environment')
   az = sh(returnStdout: true, script: 'curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone').trim()
   region = az.replaceFirst(/[a-z]$/, "")
   account = sh(returnStdout: true, script: 'curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep -oP \'(?<="accountId" : ")[^"]*(?=")\'').trim()
