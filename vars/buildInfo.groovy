10e0bc3cae8e9ecee6d2bab75243d182aa5856bd
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-05 17:38:14 +0800

    Stage name from env var

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,11 +1,11 @@
 
 
-def call(String stagename = '') {
+def call(String message = '') {
   infoSharedLib = 'jenkins-pipeline-library'
 
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
   env.SHARED_LIB = infoSharedLib
-  echo "[Pipeline] \u001B[31mStage('$stagename')\u001B[0m \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
+  echo "[Pipeline] \u001B[31mStage('env.STAGE_NAME')\u001B[0m \""$message"\" \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
 }
 
