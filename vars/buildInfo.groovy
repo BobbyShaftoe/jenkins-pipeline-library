68991e70cf034889c912237bd6d97d092e41e506
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-05 17:57:05 +0800

    Update build info method

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -5,10 +5,9 @@ def call(String messageString = '') {
 
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
-  //stagename = 'Test'
   stagename = env.STAGE_NAME
   env.SHARED_LIB = infoSharedLib
 
-  echo "[Pipeline] \u001B[31mStage('$stagename')\u001B[0m \""$messageString"\" \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
+  echo "[Pipeline] \u001B[31mStage('$stagename')\u001B[0m \"$messageString\" \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
 }
 
