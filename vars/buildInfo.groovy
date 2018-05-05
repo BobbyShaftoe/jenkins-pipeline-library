81e6962f027039355fb2c4288a2809fa8dd4deb1
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-05 17:39:48 +0800

    Message string var

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,11 +1,11 @@
 
 
-def call(String message = '') {
+def call(String messageString = '') {
   infoSharedLib = 'jenkins-pipeline-library'
 
   infoBuildNumber = env.BUILD_NUMBER
   infoJobName = env.JOB_NAME
   env.SHARED_LIB = infoSharedLib
-  echo "[Pipeline] \u001B[31mStage('env.STAGE_NAME')\u001B[0m \""$message"\" \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
+  echo "[Pipeline] \u001B[31mStage('env.STAGE_NAME')\u001B[0m \""$messageString"\" \u001B[35mSharedLib: \"$infoSharedLib\u001B[0m\""
 }
 
