e3f7a62de2ed11ac24195d18846deeb44bf72f5b
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-06 13:42:08 +0800

    Create repo dir name

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,10 +1,17 @@
 def call(String repo) {
 
+  repoName = repo.replace('^.*/([a-zA-Z0-9_-]+).git', $1)
+
   node('aws-node-00') {
     ansiColor('xterm') {
 
       try {
         dir('.') {
+          stage('Create repository directory'){
+            sh "mkdir -p $repoName"
+          }
+        }
+        dir($repoName) {
           stage('Checkout repository') {
             buildInfo("$repo")
             git poll: false, url: repo
