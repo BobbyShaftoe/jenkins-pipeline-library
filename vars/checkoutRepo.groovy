8d09307ce117a25ce85a59c4d616f6c85485554d
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-06 16:26:48 +0800

    This workspace var

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -5,29 +5,31 @@ def call(String repo) {
 
     node("$nodeName") {
 
-        customWorkspace thisWorkSpace
+        ws("$thisWorkSpace") {
 
-        ansiColor('xterm') {
+            ansiColor('xterm') {
 
-            repoName = repo.replaceAll('.*/(.*)\\.git', '\$1')
+                repoName = repo.replaceAll('.*/(.*)\\.git', '\$1')
 
-            try {
-                dir('.') {
-                    stage('Create repository directory') {
-                        buildInfo(repoName)
-                        sh "mkdir -p $repoName"
+                try {
+                    dir('.') {
+                        stage('Create repository directory') {
+                            buildInfo(repoName)
+                            sh "mkdir -p $repoName"
+                        }
                     }
-                }
-                dir('.') {
-                    stage('Checkout repository') {
-                        buildInfo("$repo")
-                        git poll: false, url: repo
+                    dir('.') {
+                        stage('Checkout repository') {
+                            buildInfo("$repo")
+                            git poll: false, url: repo
+                            //stash allowEmpty: true, name: 'server-bootstraps-ansible', useDefaultExcludes: false
+                        }
                     }
-                }
 
-            } catch (err) {
-                currentBuild.result = 'FAILED'
-                throw err
+                } catch (err) {
+                    currentBuild.result = 'FAILED'
+                    throw err
+                }
             }
         }
     }
