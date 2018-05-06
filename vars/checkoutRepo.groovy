bfb8f4eadfa9a52b1cf526885fe609d8df616f44
nicksinclair <tech@halcyonnetworks.com.au>
2018-05-06 15:46:05 +0800

    This workspace var

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,22 +1,22 @@
 def call(String repo) {
 
     nodeName = env.JOB_NODE_NAME
-    println env.WORKSPACE
+    println env.THIS_WORKSPACE
 
     node("$nodeName") {
         ansiColor('xterm') {
 
             repoName = repo.replaceAll('.*/(.*)\\.git', '\$1')
-            println env.WORKSPACE
+            println env.THIS_WORKSPACE
 
             try {
-                dir('.') {
+                dir("$env.THIS_WORKSPACE") {
                     stage('Create repository directory') {
                         buildInfo(repoName)
                         sh "mkdir -p $repoName"
                     }
                 }
-                dir("$repoName") {
+                dir("$env.THIS_WORKSPACE") {
                     stage('Checkout repository') {
                         buildInfo("$repo")
                         git poll: false, url: repo
